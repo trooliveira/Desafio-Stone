7322dcd6a718f7a34608d6f05ecee7c9a21dbe6a
Eu <trooliveira@localhost>
2022-06-10 19:01:15 -0300

    Finalizando desafio

--- reprocessamento.js
+++ reprocessamento.js
@@ -1,16 +1,41 @@
-const fixTransactions(userIds) => {
-  console.log('Corrigindo transações dos usuários enviados')
+$(document).ready(function() {
+  $.ajax({
+      type: "GET",
+      url: "users_ids.csv",
+      dataType: "text",
+      success: function(data) {processData(data);}
+   });
+});
+
+function processData(allText) {
+  var allTextLines = allText.split(/\n/);
+  var index = [];
+  var email = [];
+  for (var i=1; i < allTextLines.length; i++){
+      raw = allTextLines[i].split(';');
+  index.push(raw[0]);
+  email.push(raw[1]);
+  };
+  fixTransactions(index)
+  sendEmails(email);
+
+function fixTransactions(userIds) {
+console.log('Corrigindo transações dos usuários enviados')
 }
 
-const sendEmails(userEmails) => {
-  console.log('Enviando comunicação para os usuários impactados')
+function sendEmails(userEmails) {
+console.log('Enviando comunicação para os usuários impactados')
 }
 
-const main = async () => {
-  await fixTransactions()
-  await sendEmails()
+/* function main = async () => {
+await fixTransactions()
+await sendEmails()
+} */
+
+function main(){
+processData(allText);
+// this.then(() => console.log('Transações reprocessadas!'));
+// this.catch(err => console.error(err));
 }
 
-main()
-  .then(() => console.log('Transações reprocessadas!')
-  .catch(err => console.error(err))
+}
